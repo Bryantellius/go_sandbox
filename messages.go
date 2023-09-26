acdbb45f143c99f146fe38f4752ad4323f936635
Ben Bryant <brbryant@uabmc.edu>
2023-09-26 18:18:13 -0500

    added CreateMessages func that takes slice param

--- messages.go
+++ messages.go
@@ -3,13 +3,38 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
+
+var formats = []string{
+	"Hello %v",
+	"Hi %v",
+	"Welcome %v",
+}
 
 func CreateMessage(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("no name provided")
 	} else {
-		var message string = fmt.Sprintf("Hello, %v", name)
+		var selectedFormat string = formats[rand.Intn(len(formats))]
+		var message string = fmt.Sprintf(selectedFormat, name)
 		return message, nil
 	}
+}
+
+func CreateMessages(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for index, name := range names {
+		message, err := CreateMessage(name)
+
+		fmt.Printf("Creating message %d for %s\n", index, name)
+
+		if err != nil {
+			return nil, err
+		} else {
+			messages[name] = message
+		}
+	}
+
+	return messages, nil
 }
