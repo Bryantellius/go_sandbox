3dedd15e0fc76c0cc2de8421ec75983d18070780
Ben Bryant <brbryant2639@gmail.com>
2024-02-22 09:52:32 -0600

    fixed utils import and added addition function

--- a/main_a.go
+++ a/main_a.go
@@ -2,14 +2,20 @@ package a
 
 import (
 	"fmt"
-
+	utils "github.com/Bryantellius/go_sandbox/utils"
 	b "github.com/Bryantellius/go_sandbox/b"
 )
 
 func DoSomething() {
 	fmt.Println("Beginning A...")
 
-	fmt.Printf("A for addition. 1 + 1 = %d\n", Sum(1, 1))
+	fmt.Println("A for addition.")
+
+	addition()
 
 	b.DoSomething()
+}
+
+func addition() {
+	fmt.Printf("1 + 1 = %d\n", utils.Sum(1, 1))
 }
