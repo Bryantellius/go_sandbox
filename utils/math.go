4c5784dc7378e878e874254cfe8598e218803512
Ben Bryant <brbryant@uabmc.edu>
2024-02-22 08:56:52 -0600

    moved math and messages to utils folder

---
+++ utils/math.go
@@ -0,0 +1,27 @@
+package main
+
+import "errors"
+
+func Sum(a int, b int) int {
+	result := a + b
+	return result
+}
+
+func Subtract(a int, b int) int {
+	result := a - b
+	return result
+}
+
+func Multiply(a int, b int) int {
+	result := a * b
+	return result
+}
+
+func Divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by 0")
+	} else {
+		result := a / b
+		return result, nil
+	}
+}
