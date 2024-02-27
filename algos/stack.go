272cc82b4025b2be60a3ab15533456aa31bd86b3
Ben Bryant <brbryant@uabmc.edu>
2024-02-27 12:04:48 -0600

    added Stack example with methods

---
+++ algos/stack.go
@@ -0,0 +1,52 @@
+package algos
+
+import (
+	"fmt"
+)
+
+// Write a program to create a stack (LIFO) which can store any number of local variables for each subroutine.
+// Hint: use a linked list for the stack and local variables
+
+type LinkedList struct {
+	value string
+	next  *LinkedList
+}
+
+type Stack struct {
+	stackPointer *LinkedList
+}
+
+func CreateStack() (s *Stack) {
+	s = &Stack{stackPointer: &LinkedList{}}
+	return
+}
+
+func (s *Stack) Push(signature string) {
+	fmt.Printf("Pushing %s...\n", signature)
+	next := s.stackPointer
+	s.stackPointer = &LinkedList{value: signature, next: next}
+}
+
+func (s *Stack) Pop() string {
+	val := s.stackPointer.value
+	fmt.Printf("Popping %s...\n", val)
+	if s.stackPointer.next != nil {
+		s.stackPointer = s.stackPointer.next
+	}
+
+	return val
+}
+
+func (s *Stack) ShowStack() {
+	p := s.stackPointer
+	level := 0
+
+	for {
+		fmt.Printf("| %d: %s |\n", level, p.value)
+		p = p.next
+		level++
+		if p == nil {
+			break
+		}
+	}
+}
