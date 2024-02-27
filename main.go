35c61093c934228d95b283ebed1bccd9d64aba91
Ben Bryant <brbryant@uabmc.edu>
2024-02-27 12:05:02 -0600

    added DoCallStack method to test

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/Bryantellius/go_sandbox/a"
+import (
+	"fmt"
+
+	"github.com/Bryantellius/go_sandbox/algos"
+)
 
 func main() {
-	a.DoSomething()
+	DoCallStack()
+}
+
+func DoCallStack() {
+	fmt.Println("Starting")
+	callStack := algos.CreateStack()
+	callStack.ShowStack()
+	callStack.Push("main()")
+	callStack.ShowStack()
+	callStack.Push("DoSomething()")
+	callStack.ShowStack()
+	callStack.Pop()
+	callStack.Push("DoSomethingReallyCool()")
+	callStack.ShowStack()
+	callStack.Pop()
+	callStack.ShowStack()
+	callStack.Pop()
+	callStack.ShowStack()
+	fmt.Println("Finished")
 }
